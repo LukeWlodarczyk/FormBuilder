ea12037b6983c681f777f7e39a5e2d1ad39b8c29
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-09 04:35:36 +0200

    add Input and SubInputs components

--- src/components/InputConfig.js
+++ src/components/InputConfig.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Inputs from "./Inputs";
+import SubInputs from "./SubInputs";
+
 const condition = {
   text: {
     options: ["equals"],
@@ -32,64 +35,25 @@ const InputConfig = ({
   subInput,
   parentAnswerType
 }) => (
-  <div style={{ marginLeft: subInput ? 5 : 0 }}>
-    {subInput && (
-      <div>
-        <select
-          value={values[id].condition.type}
-          name="type"
-          onChange={handleChangeCondition}
-        >
-          {condition[parentAnswerType].options.map(i => (
-            <option key={i} value={i}>
-              {i}
-            </option>
-          ))}
-        </select>
-        {parentAnswerType !== "yes/no" ? (
-          <input
-            name="expect"
-            value={values[id].condition.expect}
-            type={condition[parentAnswerType].input.type}
-            onChange={handleChangeCondition}
-          />
-        ) : (
-          <select
-            name="expect"
-            value={values[id].condition.expect}
-            onChange={handleChangeCondition}
-          >
-            {condition[parentAnswerType].select.options.map(o => (
-              <option key={o} value={o}>
-                {o}
-              </option>
-            ))}
-          </select>
-        )}
-      </div>
-    )}
-    <input
-      name="question"
-      type="text"
-      value={values[id].question}
-      onChange={handleChange}
+  <div className="input-config" style={{ marginLeft: subInput ? 5 : 0 }}>
+    <Inputs
+      subInput={subInput}
+      values={values}
+      id={id}
+      handleChangeCondition={handleChangeCondition}
+      handleChange={handleChange}
+      condition={condition}
+      parentAnswerType={parentAnswerType}
     />
-    <select name="type" value={values[id].type} onChange={handleChange}>
-      <option value="text">Text</option>
-      <option value="number">Number</option>
-      <option value="yes/no">Yes/No</option>
-    </select>
-    <button onClick={deleteInput}>Delete</button>
-    <button onClick={addSubInput}>Add Sub Input</button>
-    {!!sub.length &&
-      sub.map(data => (
-        <InputConfig
-          key={data.id}
-          {...data}
-          {...{ parentAnswerType: values[id].type }}
-          values={values}
-        />
-      ))}
+    <div className="sub-action-btns">
+      <button className="btn" onClick={deleteInput}>
+        Delete
+      </button>
+      <button className="btn" onClick={addSubInput}>
+        Add Sub-Input
+      </button>
+    </div>
+    <SubInputs sub={sub} values={values} id={id} />
   </div>
 );
 
