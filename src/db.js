9eb2073efb94f0cdb086ed08eef2184fddb5655f
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-10-05 23:02:25 +0200

    add db class

--- src/db.js
+++ src/db.js
@@ -4,3 +4,19 @@ const db = new Dexie("FormBuilder");
 db.version(1).stores({ forms: "++id" });
 
 export default db;
+
+class DB {
+  constructor() {
+    this.db = new Dexie("FormBuilder").version(1).stores({ forms: "++id" });
+  }
+
+  getAll = () => this.db.table("forms").toArray();
+
+  getById = id => this.db.table("forms").get(id);
+
+  add = data => this.db.table("forms").add(data);
+
+  updateById = (id, data) => this.db.table("forms").update(id, data);
+
+  deleteById = id => this.db.table("forms").delete(id);
+}
