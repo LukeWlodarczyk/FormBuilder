3ffc501746883d17416dcf6fc8f157ae26e1fa99
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-10-08 04:52:14 +0200

    form preview styles

--- src/components/formpreview/PreviewInput.js
+++ src/components/formpreview/PreviewInput.js
@@ -4,30 +4,37 @@ import { shouldRenderSubs } from "../../helpers";
 
 export const PreviewInput = data => (
   <Fragment>
-    <div className="input-preview">
-      <p>{data.question}</p>
+    <div className="preview">
+      <p className="question-preview">{data.question}</p>
       {data.type !== "yes/no" ? (
         <input
           type={data.type}
           value={data.values[data.id].answer}
           onChange={data.handleChange}
+          className="input input-preview"
         />
       ) : (
         <Fragment>
-          Yes{" "}
-          <input
-            type="radio"
-            checked={data.values[data.id].answer === "Yes"}
-            value="Yes"
-            onChange={data.handleChange}
-          />
-          No{" "}
-          <input
-            type="radio"
-            checked={data.values[data.id].answer === "No"}
-            value="No"
-            onChange={data.handleChange}
-          />
+          <label className="label-preview">
+            <input
+              type="radio"
+              checked={data.values[data.id].answer === "Yes"}
+              value="Yes"
+              onChange={data.handleChange}
+              className="input-radio-preview"
+            />
+            Yes
+          </label>
+          <label className="label-preview">
+            <input
+              type="radio"
+              checked={data.values[data.id].answer === "No"}
+              value="No"
+              onChange={data.handleChange}
+              className="input-radio-preview"
+            />
+            No
+          </label>
         </Fragment>
       )}
     </div>
